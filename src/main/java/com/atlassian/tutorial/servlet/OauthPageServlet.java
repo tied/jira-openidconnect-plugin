9e26e2762c80602ea7073a7a093398853852d5fd
Dmytro Liakhov <dliakh@softserveinc.com>
2018-12-24 14:21:13 +0200

    Add new page with button of log in

--- src/main/java/com/atlassian/tutorial/servlet/OauthPageServlet.java
+++ src/main/java/com/atlassian/tutorial/servlet/OauthPageServlet.java
@@ -1,5 +1,13 @@
 package com.atlassian.tutorial.servlet;
 
+import com.atlassian.jira.bc.issue.IssueService;
+import com.atlassian.jira.bc.issue.search.SearchService;
+import com.atlassian.jira.bc.project.ProjectService;
+import com.atlassian.jira.config.ConstantsManager;
+import com.atlassian.jira.security.JiraAuthenticationContext;
+import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
+import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
+import com.atlassian.templaterenderer.TemplateRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,14 +16,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+@Scanned
 public class OauthPageServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(OauthPageServlet.class);
 
+    @JiraImport
+    private IssueService issueService;
+    @JiraImport
+    private ProjectService projectService;
+    @JiraImport
+    private SearchService searchService;
+    @JiraImport
+    private TemplateRenderer templateRenderer;
+    @JiraImport
+    private JiraAuthenticationContext authenticationContext;
+    @JiraImport
+    private ConstantsManager constantsManager;
+
+    private static final String OAUTH_LOGIN_PAGE_TEMPLATE = "/templates/login-outh.vm";
+
+    public OauthPageServlet(IssueService issueService, ProjectService projectService, SearchService searchService,
+                            TemplateRenderer templateRenderer, JiraAuthenticationContext authenticationContext, ConstantsManager constantsManager) {
+        this.issueService = issueService;
+        this.projectService = projectService;
+        this.searchService = searchService;
+        this.templateRenderer = templateRenderer;
+        this.authenticationContext = authenticationContext;
+        this.constantsManager = constantsManager;
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        resp.getWriter().write("<html><body>Hello World</body></html>");
+        Map<String, Object> context = new HashMap<>();
+        resp.setContentType("text/html;charset=utf-8");
+        templateRenderer.render(OAUTH_LOGIN_PAGE_TEMPLATE, context, resp.getWriter());
     }
 
 }
