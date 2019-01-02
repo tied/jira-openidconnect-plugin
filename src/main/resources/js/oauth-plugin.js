a7853be641c648d966ffde4409e73de0dc6663d5
Dmytro Liakhov <dliakh@softserveinc.com>
2019-01-02 13:25:08 +0200

    Update admin view page

--- src/main/resources/js/oauth-plugin.js
+++ src/main/resources/js/oauth-plugin.js
@@ -15,7 +15,7 @@
             $("#client-secret").val(config.clientSecret);
         });
 
-        AJS.$("#admin").submit(function(e) {
+        AJS.$("#admin").submit(function (e) {
             e.preventDefault();
             updateConfig();
         });
@@ -28,9 +28,34 @@
             contentType: "application/json",
             data: '{ "domain": "' + AJS.$("#domain").attr("value").trim()
                 + '", "clientId": "' + AJS.$("#client-id").attr("value").trim()
-                + '", "clientSecret": "' + AJS.$("#client-secret").attr("value").trim()  + '" }',
+                + '", "clientSecret": "' + AJS.$("#client-secret").attr("value").trim() + '" }',
             processData: false
+        }).done(function () {
+            cleanOldMessages();
+            $('#admin').parent().prepend(AJS.messages.info({
+                title: 'Open ID Connect configurations was saved successfully.'
+            }));
+            cleanMessageWithTime();
+        }).fail(function () {
+            cleanOldMessages();
+            $('#admin').parent().prepend(AJS.messages.error({
+                title: 'Cannot save data. Please, double check fields.'
+            }));
+            cleanMessageWithTime();
         });
+    }
+
+    function cleanMessageWithTime() {
+        setTimeout(function () {
+            $('#admin').parent().find('.aui-message').hide(500);
+            setTimeout(function () {
+                $('#admin').parent().find('.aui-message').remove();
+            }, 500);
+        }, 2000);
+    }
+
+    function cleanOldMessages() {
+        $('#admin').parent().find('.aui-message').remove();
     }
 
 })(AJS.$ || jQuery);
